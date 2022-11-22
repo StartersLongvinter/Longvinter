1e1b0766a0c41fc698524b7c49d3b0f3aeeb616e
jundonghyun <j950912@naver.com>
2022-11-22 17:19:18 +0900

    1122_donghyun
    - 닷트윈 최적화
    - UI툴팁 추가(수정예정)
    - 낚시기능과 인벤토리, 도감기능 합침

--- Assets/Scripts/donghyun/EncyclopediaAnimatinos.cs
+++ Assets/Scripts/donghyun/EncyclopediaAnimatinos.cs
@@ -7,30 +7,61 @@ using UnityEngine.UI;
 
 public class EncyclopediaAnimatinos : MonoBehaviour
 {
+    private Sequence initDot;
+    private Sequence enableDot;
+    private Sequence disableDot;
+
     private void Awake()
     {
-        transform.localScale = new Vector3(0, 0, 0);
-        DOTween.Sequence()
-            .Prepend(transform.DOLocalMove(Vector3.left * -100f, 0.5f));
+        // initDot = DOTween.Sequence();
+        // enableDot = DOTween.Sequence();
+        // disableDot = DOTween.Sequence();
+        //
+        // transform.localScale = new Vector3(0, 0, 0);
+        //
+        // initDot
+        //     .Prepend(transform.DOLocalMove(Vector3.left * -100f, 0.5f));
+        //
+        // enableDot
+        //     .Prepend(transform.DOScale(new Vector3(0f, 0f, 0f), 0.01f))
+        //     .Append(transform.DOScale(new Vector3(1f, 1f, 1f), 0.5f))
+        //     .Join(transform.DOLocalMove(Vector3.left * 500f, 0.5f));
+        //
+        // disableDot
+        //     .Prepend(transform.DOLocalMove(Vector3.left * -100f, 0.5f))
+        //     .Join(transform.DOScale(new Vector3(0f, 0f, 0f), 0.1f));
+        //
+        // initDot.Play();
     }
 
-    private void OnEnable()
+    private void Start()
     {
-        DOTween.Sequence()
-            .Prepend(transform.DOScale(new Vector3(0f, 0f, 0f), 0.01f))
-            .Append(transform.DOScale(new Vector3(1f, 1f, 1f), 0.5f))
+        initDot = DOTween.Sequence();
+
+        transform.localScale = new Vector3(0, 0, 0);
+        
+        initDot
+            .SetAutoKill(false)
+            .SetLink(gameObject, LinkBehaviour.RestartOnEnable)
+            .Append(transform.DOLocalMove(Vector3.left * -100f, 0.5f))
+            .Join(transform.DOScale(new Vector3(1f, 1f, 1f), 0.5f))
             .Join(transform.DOLocalMove(Vector3.left * 500f, 0.5f));
     }
 
-    private void OnDisable()
-    {
-        DOTween.Sequence()
-            .Prepend(transform.DOLocalMove(Vector3.left * -100f, 0.5f))
-            .Join(transform.DOScale(new Vector3(0f, 0f, 0f), 0.1f));
-    }
-
-    private void OnDestroy()
-    {
-        DOTween.KillAll();
-    }
+    // private void OnEnable()
+    // {
+    //     enableDot.Play();
+    // }
+    //
+    // private void OnDisable()
+    // {
+    //     disableDot.Play();
+    // }
+    //
+    // private void OnDestroy()
+    // {
+    //     initDot.Kill();
+    //     enableDot.Kill();
+    //     disableDot.Kill();
+    // }
 }
