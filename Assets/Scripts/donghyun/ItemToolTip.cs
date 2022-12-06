be6dbdc9de8336dc07804b49f332ba96e9abed30
jundonghyun <j950912@naver.com>
2022-12-06 19:36:08 +0900

    1206_donghyun
    - 아이템 자세한 내용 툴팁추가
    - 아이템 사용시 효과 플레이어 스탯으로 전달
    - 아이템 효과 만료시 알림창 띄움

--- Assets/Scripts/donghyun/ItemToolTip.cs
+++ Assets/Scripts/donghyun/ItemToolTip.cs
@@ -1,64 +1,154 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using DG.Tweening;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
-using Image = UnityEngine.UI.Image;
 
-public class ItemToolTip : MonoBehaviour, IPointerEnterHandler, IPointerMoveHandler, IPointerExitHandler
+public class ItemTooltip : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerMoveHandler, IPointerExitHandler
 {
     [SerializeField] private GameObject toolTip;
-    
-    private GameObject slot;
+    [SerializeField] private Text itemNameText;
+    [SerializeField] private Text itemExplainText;
+    [SerializeField] private GameObject explainBG;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag != null)
+        {
+            eventData.pointerDrag.transform.SetParent(transform);
+            eventData.pointerDrag.GetComponent<RectTransform>().position =
+                GetComponent<RectTransform>().position;
+        }
+    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        toolTip.gameObject.SetActive(true);
+        if (eventData.pointerCurrentRaycast.isValid &&
+            eventData.pointerCurrentRaycast.gameObject.GetComponent<Item>() != null)
+        {
+            toolTip.SetActive(true);
+            explainBG.SetActive(true);
+            
+            transform.GetChild(0).DOScale(new Vector3(1.2f, 1.2f, 1.2f), 0f);
+        }
     }
 
     public void OnPointerMove(PointerEventData eventData)
     {
-        if (eventData.pointerCurrentRaycast.isValid)
+        if (eventData.pointerCurrentRaycast.isValid && eventData.pointerCurrentRaycast.gameObject.GetComponent<Item>())
         {
-            if (eventData.pointerCurrentRaycast.gameObject.GetComponent<Item>() != null)
+            if (eventData.pointerCurrentRaycast.gameObject.GetComponent<Item>().item != null)
             {
-                toolTip.SetActive(true);
-                
-                slot = eventData.pointerCurrentRaycast.gameObject;
+                itemNameText.text = eventData.pointerCurrentRaycast.gameObject
+                    .GetComponent<Item>().item.itKorName;
 
-                slot.transform.DOScale(new Vector3(1.2f, 1.2f, 1.2f), 0f);
-
-                if (eventData.pointerCurrentRaycast.gameObject.GetComponent<Item>().item != null)
-                {
-                    toolTip.GetComponentInChildren<Text>().text = eventData.pointerCurrentRaycast.gameObject.GetComponent<Item>().item.itKorName;
-                }
-                
-                if (eventData.pointerCurrentRaycast.gameObject.GetComponent<Item>().equipment != null)
-                {
-                    toolTip.GetComponentInChildren<Text>().text = eventData.pointerCurrentRaycast.gameObject.GetComponent<Item>().equipment.emKorName;
-                }
-                
-                Vector2 vec = new Vector2(toolTip.GetComponentInChildren<Text>().preferredWidth + 30f, toolTip.GetComponentInChildren<Text>().preferredHeight);
+                Vector2 vec = new Vector2(itemNameText.preferredWidth + 30f,
+                    itemNameText.preferredHeight);
 
                 toolTip.GetComponent<RectTransform>().sizeDelta = vec;
-                
-                toolTip.GetComponent<RectTransform>().position =
-                    new Vector2(eventData.position.x + 30f, eventData.position.y + 30f);
+
+                if (vec.x + eventData.position.x >= Screen.width)
+                {
+                    toolTip.GetComponent<RectTransform>().position =
+                        new Vector2(eventData.position.x, 
+                            eventData.position.y + (vec.y));
+                }
+                else
+                {
+                    toolTip.GetComponent<RectTransform>().position =
+                        new Vector2(eventData.position.x + (vec.x / 2) + 20f, eventData.position.y + 50f);
+                }
             }
-            else
+
+            if (eventData.pointerCurrentRaycast.gameObject.GetComponent<Item>().equipment != null)
             {
-                slot.transform.DOScale(new Vector3(1f, 1f, 1f), 0f);
-                toolTip.SetActive(false);
+                itemNameText.text = eventData.pointerCurrentRaycast.gameObject
+                    .GetComponent<Item>().equipment.emKorName;
+
+                Vector2 vec = new Vector2(itemNameText.preferredWidth + 30f,
+                    itemNameText.preferredHeight);
+
+                toolTip.GetComponent<RectTransform>().sizeDelta = vec;
+
+                if (vec.x + eventData.position.x >= Screen.width)
+                {
+                    toolTip.GetComponent<RectTransform>().position =
+                        new Vector2(eventData.position.x, 
+                            eventData.position.y + (vec.y));
+                }
+                else
+                {
+                    toolTip.GetComponent<RectTransform>().position =
+                        new Vector2(eventData.position.x + (vec.x / 2) + 20f, eventData.position.y + 50f);
+                }
+            }
+        }
+        
+        if (eventData.pointerCurrentRaycast.isValid 
+            && eventData.pointerCurrentRaycast.gameObject.GetComponent<Item>())
+        {
+            itemExplainText.gameObject.SetActive(true);
+
+            if (eventData.pointerCurrentRaycast.gameObject.GetComponent<Item>().item != null)
+            {
+                itemExplainText.text = eventData.pointerCurrentRaycast.gameObject
+                    .GetComponent<Item>().item.itExplan;
+
+                Vector2 vec = new Vector2(itemExplainText.preferredWidth + 30f,
+                    itemExplainText.preferredHeight);
+                
+                explainBG.GetComponent<RectTransform>().sizeDelta = vec;
+
+                if (vec.x + eventData.position.x >= Screen.width)
+                {
+                    explainBG.GetComponent<RectTransform>().position =
+                        new Vector2(eventData.position.x, 
+                            eventData.position.y - (vec.y / 2));
+                }
+                else
+                {
+                    explainBG.GetComponent<RectTransform>().position =
+                        new Vector2(eventData.position.x + (vec.x / 2) + 20f, eventData.position.y - (vec.y / 2));
+                }
+            }
+
+            if (eventData.pointerCurrentRaycast.gameObject.GetComponent<Item>().equipment != null)
+            {
+                itemExplainText.text = eventData.pointerCurrentRaycast.gameObject
+                    .GetComponent<Item>().equipment.emKorName;
+
+                Vector2 vec = new Vector2(itemExplainText.preferredWidth + 30f,
+                    itemExplainText.preferredHeight);
+
+                explainBG.GetComponent<RectTransform>().sizeDelta = vec;
+
+                if (vec.x + eventData.position.x >= Screen.width)
+                {
+                    explainBG.GetComponent<RectTransform>().position =
+                        new Vector2(eventData.position.x, 
+                            eventData.position.y - (vec.y / 2));
+                }
+                else
+                {
+                    explainBG.GetComponent<RectTransform>().position =
+                        new Vector2(eventData.position.x + (vec.x / 2) + 20f, eventData.position.y - (vec.y / 2));
+                }
             }
         }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        slot = null;
-        toolTip.gameObject.SetActive(false);
+        toolTip.SetActive(false);
+        explainBG.SetActive(false);
+        
+        itemExplainText.text = String.Empty;
+
+        if (transform.childCount != 0)
+        {
+            transform.GetChild(0).DOScale(new Vector3(1f, 1f, 1f), 0f);
+        }
     }
 }
